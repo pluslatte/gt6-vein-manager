c8c8211a6a4334bc0fd0196f330a0c8df86861e7
pluslatte <debrisfield0201@gmail.com>
2025-06-03 07:03:05 +0900

    ディレクトリ名の変更

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,9 +1,9 @@
-pub mod api;
-pub mod web;
-pub mod static_files;
 pub mod auth;
+pub mod static_files;
+pub mod vein;
+pub mod web;
 
-pub use api::*;
-pub use web::*;
-pub use static_files::*;
 pub use auth::*;
+pub use static_files::*;
+pub use vein::*;
+pub use web::*;
