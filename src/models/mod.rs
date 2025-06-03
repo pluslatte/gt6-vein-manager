e0f830966f92f07b24613f45cf1f5f80489635ce
pluslatte <debrisfield0201@gmail.com>
2025-06-04 05:48:40 +0900

    セッションテーブルのモデル

--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,7 +1,9 @@
 pub mod auth;
 pub mod forms;
+pub mod sessions;
 pub mod vein;
 
 pub use auth::*;
 pub use forms::*;
+pub use sessions::*;
 pub use vein::*;
---
+++ src/models/sessions.rs
@@ -0,0 +1,19 @@
+use chrono::NaiveDateTime;
+use diesel::prelude::*;
+
+#[derive(Queryable, Selectable, Insertable, AsChangeset, Debug, Clone)]
+#[diesel(table_name = gt6_vein_manager::schema::sessions)]
+#[diesel(check_for_backend(diesel::mysql::Mysql))]
+pub struct Session {
+    pub id: String,
+    pub data: String,
+    pub expiry_date: NaiveDateTime,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = gt6_vein_manager::schema::sessions)]
+pub struct NewSession {
+    pub id: String,
+    pub data: String,
+    pub expiry_date: NaiveDateTime,
+}
